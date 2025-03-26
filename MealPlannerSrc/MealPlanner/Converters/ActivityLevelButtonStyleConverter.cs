2db7bc75a05639566637966d50ac426e7a06f4d8
916-Solomie-Rebeca <rebeca.solomie@stud.ubbcluj.ro>
2025-03-26 16:06:53 +0200

    Create ActivityLevelButtonStyleConverter.cs

---
+++ MealPlannerSrc/MealPlanner/Converters/ActivityLevelButtonStyleConverter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Controls;
+using System;
+
+namespace MealPlanner.Converters
+{
+    public class ActivityLevelButtonStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string activityLevel)
+            {
+                switch (activityLevel)
+                {
+                    case "Sedentary":
+                        return Application.Current.Resources["SedentaryButtonStyle"] as Style;
+                    case "Lightly Active":
+                        return Application.Current.Resources["LightlyActiveButtonStyle"] as Style;
+                    case "Moderately Active":
+                        return Application.Current.Resources["ModeratelyActiveButtonStyle"] as Style;
+                    case "Very Active":
+                        return Application.Current.Resources["VeryActiveButtonStyle"] as Style;
+                    case "Super Active":
+                        return Application.Current.Resources["SuperActiveButtonStyle"] as Style;
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
