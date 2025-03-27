b7ab40a307d48a06a8623d760214919924cfcfd9
916-Stroia-Carina-Raluca <carina.stroia@stud.ubbcluj.ro>
2025-03-27 22:14:30 +0200

    commit 2 Ralu
    The initial pages, no database
    + I still have to work on the measurement units

---
+++ MealPlannerSrc/MealPlanner/ViewModels/BodyMetricsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MealPlanner.Pages;
+using MealPlanner.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using System.Collections.ObjectModel;
+using MealPlanner.Pages;
+
+namespace MealPlanner.ViewModels
+{
+    public class BodyMetricsViewModel : ViewModelBase
+    {
+        
+        public ICommand NextCommand { get; }
+
+        public BodyMetricsViewModel()
+        {
+            NextCommand = new RelayCommand(GoNext);
+        }
+
+
+        private void GoNext()
+        {
+            NavigationService.Instance.NavigateTo(typeof(GoalPage));
+        }
+
+
+    }
+}
