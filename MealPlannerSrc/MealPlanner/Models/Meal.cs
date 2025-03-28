f9a07cc1dfd228bf60fd041fbb430c386f3146d8
916-savoiu-larisa <146817915+916-savoiu-larisa@users.noreply.github.com>
2025-03-28 16:59:50 +0200

    commit

---
+++ MealPlannerSrc/MealPlanner/Models/Meal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Models
+{
+    public class Meal
+    {
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public int Calories { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Recipe { get; set; }
+
+        public Meal(string name, string ingredients, int calories, string category, string imagePath, string recipe)
+        {
+            Name = name;
+            Ingredients = ingredients;
+            Calories = calories;
+            Category = category;
+            ImagePath = imagePath;
+            Recipe = recipe;
+        }
+
+        public Meal() { }
+    }
+}
