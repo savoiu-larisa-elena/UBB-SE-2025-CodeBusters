b16c6c593f9f21a627c372c5650b536b8a7784e2
916-Stroia-Carina-Raluca <carina.stroia@stud.ubbcluj.ro>
2025-04-01 01:41:55 +0300

    commit

---
+++ MealPlannerSrc/MealPlanner/Models/IngredientModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Models
+{
+    public class IngredientModel
+    {
+
+        //i_id int primary key identity(1,1),
+        //u_id int foreign key references users(u_id), -- can be null
+        //i_name varchar(150) not null,
+        //category varchar(150) not null,
+        //protein float,
+        //calories float,
+        //carbohydrates float,
+        //fat float,
+        //fiber float,
+        //sugar float
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public float Protein { get; set; }
+        public float Calories { get; set; }
+        public float Carbohydrates { get; set; }
+        public float Fat { get; set; }
+        public float Fiber { get; set; }
+        public float Sugar { get; set; }
+
+    }
+}
--- MealPlannerSrc/MealPlanner/ViewModels/AddFoodPageViewModel.cs
+++ MealPlannerSrc/MealPlanner/ViewModels/AddFoodPageViewModel.cs
@@ -10,6 +10,7 @@ using MealPlanner.Services;
 
 using System.Collections.ObjectModel;
 using MealPlanner.Pages;
+using Microsoft.Data.SqlClient;
 
 
 
@@ -18,20 +19,27 @@ namespace MealPlanner.ViewModels
 {
     public class AddFoodPageViewModel : ViewModelBase
     {
+        private string _searchText;
+        private object _selectedItem;
+        private bool _isSearchVisible;
+        private readonly string connectionString = 
+            @"Server=RALUUU_LPT\SQLEXPRESS;Database=MealPlanner;Integrated Security=True;TrustServerCertificate=True";
+
+
+        public ObservableCollection<object> SearchResults { get; set; } = new ObservableCollection<object>();
 
 
         // FOR BACK COMMAND <-
-
         public AddFoodPageViewModel() 
         {
             BackCommand = new RelayCommand(GoBack);
             NextCommand = new RelayCommand(GoNext);
+            AddToMealCommand = new RelayCommand(AddToMeal, CanAddToMeal);
+
 
         }
 
         public ICommand NextCommand { get; }
-
-
         public ICommand BackCommand { get; }
 
         private void GoBack()
@@ -43,6 +51,143 @@ namespace MealPlanner.ViewModels
         {
             NavigationService.Instance.NavigateTo(typeof(MainPage));
         }
+
+
+        // for SEARCH BAR INGREDIENT / MEAL
+
+        public ICommand AddToMealCommand { get; }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged();
+                PerformSearch();
+            }
+        }
+
+        public object SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsSearchVisible
+        {
+            get => _isSearchVisible;
+            set
+            {
+                _isSearchVisible = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private void PerformSearch()
+        {
+            SearchResults.Clear();
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                IsSearchVisible = false;
+                return;
+            }
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Search Meals first
+                string mealQuery = "SELECT m_id, m_name FROM meals WHERE m_name LIKE @search + '%'";
+                using (SqlCommand cmd = new SqlCommand(mealQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@search", SearchText);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            SearchResults.Add(new { Id = reader["m_id"], Name = reader["m_name"], Type = "Meal" });
+                        }
+                    }
+                }
+
+                // If no meals found, search Ingredients
+                if (SearchResults.Count == 0)
+                {
+                    string ingredientQuery = "SELECT i_id, i_name FROM ingredients WHERE i_name LIKE @search + '%'";
+                    using (SqlCommand cmd = new SqlCommand(ingredientQuery, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@search", SearchText);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                SearchResults.Add(new { Id = reader["i_id"], Name = reader["i_name"], Type = "Ingredient" });
+                            }
+                        }
+                    }
+                }
+            }
+
+            IsSearchVisible = SearchResults.Count > 0;
+        }
+
+        private bool CanAddToMeal() => SelectedItem != null;
+
+
+        private void AddToMeal()
+        {
+            if (SelectedItem == null) return;
+
+            int selectedId = (int)SelectedItem.GetType().GetProperty("Id")?.GetValue(SelectedItem);
+            string type = (string)SelectedItem.GetType().GetProperty("Type")?.GetValue(SelectedItem);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string insertQuery;
+
+                if (type == "Meal")
+                {
+                    insertQuery = "INSERT INTO meal_ingredients (meal_id, ingredient_id, quantity) VALUES (@mealId, NULL, 1)";
+                }
+                else // Ingredient
+                {
+                    insertQuery = "INSERT INTO meal_ingredients (meal_id, ingredient_id, quantity) VALUES (NULL, @ingredientId, 1)";
+                }
+
+                using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
+                {
+                    if (type == "Meal")
+                    {
+                        cmd.Parameters.AddWithValue("@mealId", selectedId);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@ingredientId", selectedId);
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            // Reset selection
+            SelectedItem = null;
+            SearchText = "";
+            IsSearchVisible = false;
+        }
+
+
+
+
+
+
+
 
         private int servingsCount = 0;
         public string ServingsCount
