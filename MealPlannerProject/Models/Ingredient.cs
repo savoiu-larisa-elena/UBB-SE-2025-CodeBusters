17cb999e7d3c410035fa91f489d8515b1c6fe3fd
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-04-11 20:32:39 +0300

    water and ingredient fix

--- MealPlannerProject/Models/Ingredient.cs
+++ MealPlannerProject/Models/Ingredient.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace MealPlannerProject.Models
+﻿namespace MealPlannerProject.Models
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     [Table("ingredients")]
     public class Ingredient
     {
