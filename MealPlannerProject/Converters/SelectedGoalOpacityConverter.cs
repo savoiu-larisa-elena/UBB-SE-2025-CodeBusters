2467fcdc02afebacc156fc4fb10ae587cddc8c9f
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-04-11 20:42:11 +0300

    GoalOpacity?? fixed with style cop but missing some implementation

--- MealPlannerProject/Converters/SelectedGoalOpacityConverter.cs
+++ MealPlannerProject/Converters/SelectedGoalOpacityConverter.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml;
-using MealPlannerProject;
-
-namespace MealPlannerProject.Converters
+﻿namespace MealPlannerProject.Converters
 {
+    using System;
+    using MealPlannerProject;
+    using Microsoft.UI.Xaml;
+    using Microsoft.UI.Xaml.Data;
+
     public class SelectedGoalOpacityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
@@ -13,6 +13,9 @@ namespace MealPlannerProject.Converters
             return value?.ToString() == selectedGoal ? 0.5 : 1.0;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
