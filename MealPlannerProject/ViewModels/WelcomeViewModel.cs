fdc9899e198fdf4849fcc4707d6c9691a4196323
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-04-11 18:40:57 +0300

    configured stylecop and appsettings db connection

--- MealPlannerProject/ViewModels/WelcomeViewModel.cs
+++ MealPlannerProject/ViewModels/WelcomeViewModel.cs
@@ -1,20 +1,25 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows.Input;
-using MealPlannerProject.Services;
-using MealPlannerProject.Pages;
+﻿// <copyright file="WelcomeViewModel.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace MealPlannerProject.ViewModels
 {
-    public class WelcomeViewModel : ViewModelBase
-    {
-        public ICommand GetStartedCommand { get; }
-        public ObservableCollection<string> Items { get; }
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using MealPlannerProject.Pages;
+    using MealPlannerProject.Services;
 
+    public partial class WelcomeViewModel : ViewModelBase
+    {
         public WelcomeViewModel()
         {
-            GetStartedCommand = new RelayCommand(OnGetStarted);
-            Items = new ObservableCollection<string>();
+            this.GetStartedCommand = new RelayCommand(this.OnGetStarted);
+            this.Items = new ObservableCollection<string>();
         }
+
+        public ICommand GetStartedCommand { get; }
+
+        public ObservableCollection<string> Items { get; }
 
         private void OnGetStarted()
         {
