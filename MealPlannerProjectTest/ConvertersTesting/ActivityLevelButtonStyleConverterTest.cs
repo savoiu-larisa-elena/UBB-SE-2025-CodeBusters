9f63a1300239903192817d7e612ffa8edcbc9dc4
MihaiTopi <mihai.topor@outlook.com>
2025-04-15 00:13:11 +0300

    all tests have been created

---
+++ MealPlannerProjectTest/ConvertersTesting/ActivityLevelButtonStyleConverterTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MealPlannerProject.Converters;
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+
+namespace MealPlannerProjectTest.ConverterTesting
+{
+    [TestClass]
+    public class ActivityLevelButtonStyleConverterTests
+    {
+        private ActivityLevelButtonStyleConverter? _converter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _converter = new ActivityLevelButtonStyleConverter();
+
+            // Setup fake resources for test environment
+            var resources = new ResourceDictionary
+            {
+                ["SedentaryButtonStyle"] = new Style(),
+                ["LightlyActiveButtonStyle"] = new Style(),
+                ["ModeratelyActiveButtonStyle"] = new Style(),
+                ["VeryActiveButtonStyle"] = new Style(),
+                ["SuperActiveButtonStyle"] = new Style()
+            };
+
+            Application.Start((p) => new Microsoft.UI.Xaml.Application { Resources = resources });
+        }
+
+        [TestMethod]
+        public void Convert_Sedentary_ReturnsCorrectStyle()
+        {
+            var result = _converter.Convert("Sedentary", typeof(Style), null, "en-US");
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Style));
+        }
+
+        [TestMethod]
+        public void Convert_LightlyActive_ReturnsCorrectStyle()
+        {
+            var result = _converter.Convert("Lightly Active", typeof(Style), null, "en-US");
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Convert_UnknownValue_ReturnsNull()
+        {
+            var result = _converter.Convert("Unrealistically Active", typeof(Style), null, "en-US");
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void Convert_NonStringValue_ReturnsNull()
+        {
+            var result = _converter.Convert(42, typeof(Style), null, "en-US");
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ConvertBack_Always_ThrowsNotImplementedException()
+        {
+            _converter.ConvertBack(new object(), typeof(string), null, "en-US");
+        }
+    }
+}
---
+++ MealPlannerProjectTest/ConvertersTesting/BooleanToVisibilityConverterTest.cs
@@ -0,0 +1,96 @@
+﻿namespace MealPlannerProjectTest.ConverterTesting
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.UI.Xaml;
+    using MealPlannerProject.Converters;
+
+    [TestClass]
+    public class BooleanToVisibilityConverterTests
+    {
+        private BooleanToVisibilityConverter _converter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _converter = new BooleanToVisibilityConverter();
+        }
+
+        [TestMethod]
+        public void Convert_ShouldReturnVisible_WhenValueIsTrue()
+        {
+            // Arrange
+            object value = true;
+
+            // Act
+            var result = _converter.Convert(value, typeof(Visibility), null, null);
+
+            // Assert
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [TestMethod]
+        public void Convert_ShouldReturnCollapsed_WhenValueIsFalse()
+        {
+            // Arrange
+            object value = false;
+
+            // Act
+            var result = _converter.Convert(value, typeof(Visibility), null, null);
+
+            // Assert
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        [TestMethod]
+        public void Convert_ShouldReturnCollapsed_WhenValueIsNotBoolean()
+        {
+            // Arrange
+            object value = "not a bool";
+
+            // Act
+            var result = _converter.Convert(value, typeof(Visibility), null, null);
+
+            // Assert
+            Assert.AreEqual(Visibility.Collapsed, result);
+        }
+
+        [TestMethod]
+        public void ConvertBack_ShouldReturnTrue_WhenVisibilityIsVisible()
+        {
+            // Arrange
+            object value = Visibility.Visible;
+
+            // Act
+            var result = _converter.ConvertBack(value, typeof(bool), null, null);
+
+            // Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void ConvertBack_ShouldReturnFalse_WhenVisibilityIsCollapsed()
+        {
+            // Arrange
+            object value = Visibility.Collapsed;
+
+            // Act
+            var result = _converter.ConvertBack(value, typeof(bool), null, null);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void ConvertBack_ShouldReturnFalse_WhenValueIsNotVisibility()
+        {
+            // Arrange
+            object value = "not a visibility";
+
+            // Act
+            var result = _converter.ConvertBack(value, typeof(bool), null, null);
+
+            // Assert
+            Assert.AreEqual(false, result);
+        }
+    }
+}
