aa5983a1f55234c55b38422f44513ad453b15899
Topor Mihai <147518933+MihaiTopi@users.noreply.github.com>
2025-04-15 03:33:31 +0300

    Revert "Mitzao"

--- MealPlannerProject/Exceptions/DatabaseOperationException.cs
+++ MealPlannerProject/Exceptions/DatabaseOperationException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MealPlannerProject.Exceptions
 {
-    public class DatabaseOperationException : Exception
+    class DatabaseOperationException : Exception
     {
         public DatabaseOperationException() { }
 
--- MealPlannerProject/Queries/DataLink.cs
+++ MealPlannerProject/Queries/DataLink.cs
@@ -1,16 +1,12 @@
-
 ﻿namespace MealPlannerProject.Queries
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Data.SqlClient;
-    using Microsoft.Extensions.Configuration;
-    using MealPlannerProject.Exceptions;
     using System.Data;
-    using MealPlannerProject.Interfaces.Services;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+    using MealPlannerProject.Exceptions;
+    using MealPlannerProject.Interfaces;
+    using Microsoft.Extensions.Configuration;
 
     public sealed partial class DataLink : IDisposable, IDataLink
     {
